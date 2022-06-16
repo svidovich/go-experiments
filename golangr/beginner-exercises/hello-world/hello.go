6c144074e75d301704f3dfc40007ab105e4f52b6
sam@paimon <samuel.vidovich@gmail.com>
2022-06-15 21:37:19 -0400

    Also use a variable lol.

--- golangr/beginner-exercises/hello-world/hello.go
+++ golangr/beginner-exercises/hello-world/hello.go
@@ -6,4 +6,6 @@ func main() {
     fmt.Println("HOWDY")
     fmt.Println("So what, do I just like put multiple lines?")
     fmt.Println("The website says to make this print my address. I'm not doing that.")
+    var the_string = "Chump"
+    fmt.Println(the_string)
 }
