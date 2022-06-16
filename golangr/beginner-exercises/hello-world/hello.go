f59ac268f830f3ddb1513daf3735c3f0b5edb121
sam@paimon <samuel.vidovich@gmail.com>
2022-06-15 21:35:39 -0400

    Hello dawg

---
+++ golangr/beginner-exercises/hello-world/hello.go
@@ -0,0 +1,9 @@
+package main // interesting that it's a package rn
+
+import "fmt" // wat.
+
+func main() {
+    fmt.Println("HOWDY")
+    fmt.Println("So what, do I just like put multiple lines?")
+    fmt.Println("The website says to make this print my address. I'm not doing that.")
+}
