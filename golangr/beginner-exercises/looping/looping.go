5434a67f3b3e7ea733e6fc53e0eb6e3ddb6fe9c9
sam@paimon <samuel.vidovich@gmail.com>
2022-06-18 21:50:35 -0400

    How to write a function?

--- golangr/beginner-exercises/looping/looping.go
+++ golangr/beginner-exercises/looping/looping.go
@@ -1,9 +1,15 @@
 package main
 
 import "fmt"
+import "math"
+
+func power(x float64, exponent float64) float64 {
+  return math.Pow(x, exponent)
+}
 
 func main() {
   for i := 0; i < 10; i ++ {
     fmt.Printf("Iteration: %d\n", i)
   }
+  fmt.Println(power(2, 8))
 }
