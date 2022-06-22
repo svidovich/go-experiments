db97028613364163896c017f044b0c7ebc1520e5
sam@paimon <samuel.vidovich@gmail.com>
2022-06-21 22:42:06 -0400

    Reading file contents

--- golangr/beginner-exercises/file-axxess/files.go
+++ golangr/beginner-exercises/file-axxess/files.go
@@ -3,16 +3,24 @@ package main
 import (
   "fmt"
   "os"
+  "io/ioutil" // How does this work?
 )
 
 func main() {
+  file_name := "the_file"
   // I think error is a keyword; it seems to highlight in vim
   // I also wish this tutorial would say what the first return
   // value was... ????
   // ALSO, this control flow sux lmao
-  if something, err := os.Stat("the_file"); os.IsNotExist(err) { // the fuck is this
+  if something, err := os.Stat(file_name); os.IsNotExist(err) { // the fuck is this
     fmt.Printf("The file does not exist! Also, %s\n", something)
   } else {
-    fmt.Printf("The file exists! Also %s\n", something)
+    file_bytes, err := ioutil.ReadFile(file_name)
+    if err != nil {
+      fmt.Printf("Error reading file: %s", err)
+      return
+    }
+    file_bytes_as_string := string(file_bytes)
+    fmt.Println(file_bytes_as_string)
   }
 }
